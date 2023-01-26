a49ee7eefccad1d11650907f05b77b7b6f155e87
mc_fdc <Masato.04.11.2007@gmail.com>
2023-01-26 02:27:55 +0000

    fix

---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+from voicevox import Client
+
+
+@pytest.mark.asyncio
+async def test_basic():
+    async with Client() as client:
+        audio_query = await client.create_audio_query(
+            "こんにちは！", speaker=1
+        )
+        await audio_query.synthesis()
