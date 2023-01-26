7e88470a14fc66f534f106ff40a834ad5378e019
mc_fdc <Masato.04.11.2007@gmail.com>
2023-01-26 11:10:46 +0900

    add: custom

--- voicevox/audio_query.py
+++ voicevox/audio_query.py
@@ -10,12 +10,38 @@ from .http import HttpClient
 class AudioQuery:
 
     def __init__(
-        self, http: HttpClient, audio_query: AudioQueryType,
+        self, http: HttpClient, payload: AudioQueryType,
         speaker: int
     ):
         self.http = http
-        self.data = audio_query
+        self.data = payload
+
+        self.accent_phrases = payload["accent_phrases"]
+        self.speed_scale: int = payload["speedScale"]
+        self.pitch_scale: int = payload["pitchScale"]
+        self.intonation_scale: int = payload["intonationScale"]
+        self.volume_scale: int = payload["volumeScale"]
+        self.pre_phoneme_length: int = payload["prePhonemeLength"]
+        self.post_phoneme_length: int = payload["postPhonemeLength"]
+        self.output_sampling_rate: int = payload["outputSamplingRate"]
+        self.output_stereo: bool = payload["outputStereo"]
+        self.kana: str = payload["kana"]
+
         self.speaker = speaker
+
+    def to_dict(self) -> AudioQueryType:
+        return {
+            "accent_phrases": self.accent_phrases,
+            "speedScale": self.speed_scale,
+            "pitchScale": self.pitch_scale,
+            "intonationScale": self.intonation_scale,
+            "volumeScale": self.volume_scale,
+            "prePhonemeLength": self.pre_phoneme_length,
+            "postPhonemeLength": self.post_phoneme_length,
+            "outputSamplingRate": self.output_sampling_rate,
+            "outputStereo": self.output_stereo,
+            "kana": self.kana
+        }
 
     async def synthesis(
         self, *, enable_interrogative_upspeak: bool = True,
@@ -27,4 +53,4 @@ class AudioQuery:
         }
         if core_version is not None:
             params["core_version"] = core_version
-        return await self.http.synthesis(params, self.data)
+        return await self.http.synthesis(params, self.to_dict())
