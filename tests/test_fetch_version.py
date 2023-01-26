e77964d034b286b14729ea8325ad18eb2d7c5468
mc_fdc <Masato.04.11.2007@gmail.com>
2023-01-26 13:31:45 +0900

    add: fetch core versions

--- tests/test_fetch_version.py
+++ tests/test_fetch_version.py
@@ -6,4 +6,6 @@ from voicevox import Client
 @pytest.mark.asyncio
 async def test_version():
     async with Client() as client:
-        print(await client.fetch_engine_version())
+        await client.fetch_engine_version()
+
+        await client.fetch_core_versions()
