757b850bf581fbaa38e4fc852b351bf0f01b1ff3
mc_fdc <Masato.04.11.2007@gmail.com>
2023-03-13 10:12:28 +0900

    Update client.py

--- voicevox/client.py
+++ voicevox/client.py
@@ -22,6 +22,8 @@ class Client:
     ----------
     http: HttpClient
         Http client attribute.
+    timeout: Optional[int]
+        You can customize timeout. If you use cpu mode, I recommend to use this.
     """
 
     def __init__(self, base_url: str = "http://localhost:50021", timeout: Optional[int] = None):
