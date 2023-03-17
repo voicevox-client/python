dbf217f319a7760644fba40ee09e3f809eb121eb
mc_fdc <Masato.04.11.2007@gmail.com>
2023-03-17 15:29:30 +0900

    Fix container ports

--- tests/test-with-docker.py
+++ tests/test-with-docker.py
@@ -8,7 +8,10 @@ client = docker.from_env()
 
 @pytest.mark.asyncio
 async def test_basic():
-    container = client.containers.run("voicevox/voicevox_engine:cpu-ubuntu20.04-latest", detach=True)
+    container = client.containers.run(
+        "voicevox/voicevox_engine:cpu-ubuntu20.04-latest",
+        detach=True, ports={"50021/tcp": ("127.0.0.1", 50021)}
+    )
     async with Client() as cli:
         audio_query = await cli.create_audio_query(
             "こんにちは！", speaker=1
